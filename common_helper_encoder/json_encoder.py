3c7a13092184f279b1f82308d14e42ee84d74ee0
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2016-04-29 09:21:41 +0200

    PEP8!

--- common_helper_encoder/json_encoder.py
+++ common_helper_encoder/json_encoder.py
@@ -1,8 +1,9 @@
 import json
 import datetime
 
+
 class ReportEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
             return obj.isoformat()
-        return json.JSONEncoder.default(self,obj)
+        return json.JSONEncoder.default(self, obj)
