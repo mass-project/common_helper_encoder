fc315c8e3fe1ebfffd9928e048b65903631dfaff
Timm Behner <behner@cs.uni-bonn.de>
2016-05-03 11:46:15 +0200

    Added datetime.datetime encoding test

--- tests/test_json_encoder.py
+++ tests/test_json_encoder.py
@@ -1,5 +1,6 @@
 import unittest
 from common_helper_encoder.json_encoder import ReportEncoder
+import datetime
 
 
 class Test_json_encoder(unittest.TestCase):
@@ -11,6 +12,11 @@ class Test_json_encoder(unittest.TestCase):
         input_data = b'TEST'
         result = self.encoder.default(input_data)
         self.assertEqual(result, "VEVTVA==", "base64 encoding not correct")
+
+    def test_datetime_encoding(self):
+        input_data = datetime.datetime(1970, 4, 23, 19, 00, 24)
+        result = self.encoder.default(input_data)
+        self.assertEqual(result, "1970-04-23T19:00:24")
 
 
 if __name__ == "__main__":
