75580b078e80e019855a4594ba16c5f2163f33d2
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2016-05-04 08:29:44 +0200

    test folder moved; 00 changed to 0 because of editor parsing bugs

--- common_helper_encoder/tests/test_json_encoder.py
+++ common_helper_encoder/tests/test_json_encoder.py
@@ -14,7 +14,7 @@ class Test_json_encoder(unittest.TestCase):
         self.assertEqual(result, "VEVTVA==", "base64 encoding not correct")
 
     def test_datetime_encoding(self):
-        input_data = datetime.datetime(1970, 4, 23, 19, 00, 24)
+        input_data = datetime.datetime(1970, 4, 23, 19, 0, 24)
         result = self.encoder.default(input_data)
         self.assertEqual(result, "1970-04-23T19:00:24")
 
